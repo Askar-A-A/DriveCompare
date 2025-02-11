0a0e9b3f28630740983d031af13bef49cdd6401c
Askar-A-A <askar.a.a.dev@gmail.com>
2025-02-11 22:15:23 +0500

    Set up navbar, footer and landing page

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,4 +4,4 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return render_template('main/index.html', title='Home') 
+    return render_template('pages/home.html', title='Home') 
