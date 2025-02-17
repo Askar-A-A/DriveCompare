764579d6c1cdb984369828c498b3e440384290a3
Askar-A-A <askar.a.a.dev@gmail.com>
2025-02-18 00:38:35 +0500

    PostgreSQL setup

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ load_dotenv()  # Load environment variables from .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://localhost/fuelcompare'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Add other configuration variables
